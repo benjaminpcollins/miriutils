6a290bb07adbdbd418cd3ddc5ea3b8c363d69b35
Benjamin Philip Collins <benjamin.p.collins@icloud.com>
2026-01-02 14:07:52 +0100

    Deleted a lot of functions that are gonna be remade now.

--- src/miri_utils/__init__.py
+++ src/miri_utils/__init__.py
@@ -1,8 +1,8 @@
 # __init__.py for the miri_utils package
 
 from .cutout_tools import load_cutout, produce_cutouts, calculate_angle, rotate_cutouts, resample_cutout
-from .astrometry_utils import compute_centroid, save_alignment_figure, compute_offset, write_offset_stats, visualise_offsets, shift_miri_fits, get_mean_stats, \
-    plot_offsets_polar, get_path, generate_flag_sheet, get_survey_stats, generate_master_summary
+from .astrometry_utils import compute_centroid, save_alignment_figure, compute_offset, get_path, generate_flag_sheet, get_survey_stats, \
+    generate_master_summary
 from .photometry_tools import adjust_aperture, estimate_background, get_psf, get_aperture_params, calculate_aperture_correction, measure_flux, \
     perform_photometry, create_fits_table_from_csv, compare_aperture_statistics, write_detection_stats, plot_galaxy_filter_matrix, \
         save_vis, load_vis, create_mosaics, plot_aperture_comparison, write_aperture_summary, plot_aperture_summary, plot_appendix_figure, \
